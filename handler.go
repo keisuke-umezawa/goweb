2b38f297d23450170a45b0a21a2ce86c9a8a0bcd
keisuke-umezawa <keisuke.umezawa@gmail.com>
2017-08-12 15:58:15 +0900

    add jsonErro

--- handler.go
+++ handler.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"fmt"
 	"net/http"
+    "strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -24,7 +25,26 @@ func UserIndex(w http.ResponseWriter, r *http.Request) {
 
 func UserShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	fmt.Fprintf(w, "%s user index\n", vars["userId"])
+    todoId, err := strconv.Atoi(vars["userId"]);
+    if err != nil {
+        panic(err)
+    }
+    user := RepoFindUser(todoId)
+    if user.Id > 0 {
+        w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+        w.WriteHeader(http.StatusOK)
+        if err := json.NewEncoder(w).Encode(user); err != nil {
+            panic(err)
+        }
+        return
+    }
+
+    // if we do not find it. 404.
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusNotFound)
+    if err := json.NewEncoder(w).Encode(jsonErr{Code: http.StatusNotFound, Text: "Not Found"}); err != nil {
+        panic(err)
+    }
 }
 
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +52,6 @@ func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func UserCreate(w http.ResponseWriter, r *http.Request) {
-    var user User
     body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
     if err != nil {
         panic(err)
@@ -40,6 +59,7 @@ func UserCreate(w http.ResponseWriter, r *http.Request) {
     if err := r.Body.Close(); err != nil {
         panic(err)
     }
+    var user User
     if err := json.Unmarshal(body, &user); err != nil {
         w.Header().Set("Content-Type", "application/json; charset=UTF-8")
         w.WriteHeader(422) // unprocessable entity
