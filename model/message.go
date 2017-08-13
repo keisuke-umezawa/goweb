7f9a0cb880804f6126713ed558837bea543549d9
keisuke-umezawa <keisuke.umezawa@gmail.com>
2017-08-13 23:44:58 +0900

    set FK

--- model/message.go
+++ model/message.go
@@ -6,7 +6,9 @@ import (
 
 type Message struct {
     gorm.Model
+    Sender      User        `gorm:"ForeignKey:SenderID"`
     SenderID    uint
+    Group       Group
     GroupID     uint
     Text        string      `json:"text" form:"text"`
     Mode        string      `json:"mode" form:"mode"`
